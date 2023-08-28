a1663b97cf583f12fcfab05e6f33b1de254a809d
AliReza <ali.reza.buet@gmail.com>
2023-08-28 23:36:56 +0600

    Complete increase and decrease reducer

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ChevronDown, ChevronUp } from '../icons';
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 import { useDispatch } from 'react-redux';
 
 const CartItem = ({ id, img, title, price, amount }) => {
@@ -14,11 +14,19 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <button className='remove-btn' onClick={()=> dispatch(removeItem(id))}>remove</button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => dispatch(increase({id}))}>
           <ChevronUp />
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        <button className='amount-btn' 
+          onClick={() => {
+            if (amount === 1){
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({id}))
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -21,10 +21,22 @@ const cartSlice = createSlice({
             state.cartItems = state.cartItems.filter((item)=>
             item.id !== itemId)
         },
+        increase: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => 
+                item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+            // console.log('The cartItem', cartItem);
+        },
+        decrease: (state, {payload}) => {
+            const cartItem = state.cartItems.find((item) => 
+                item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+            // console.log('The cartItem', cartItem);
+        },
     }
 });
 
 // console.log(cartSlice)
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer
