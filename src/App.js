3bb43e29b6b6a07297a0c3a9920df29bd1c9c6e9
AliReza <ali.reza.buet@gmail.com>
2023-08-28 15:07:21 +0600

    Add icons. Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Cart Item app Redux understand</h1>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import {CartIcon} from '../icons'
+import { UseSelector } from 'react-redux/es/hooks/useSelector';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='nav-center'>
+                <h3>redux toolkit</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
