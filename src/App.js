78719a96d371e21e8dda4f15db4284f00e082db1
AliReza <ali.reza.buet@gmail.com>
2023-08-28 22:07:36 +0600

    Add the items to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CartContainer />
     </div>
   );
 }
