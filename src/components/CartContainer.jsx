4974f501533e5ad5396221bd1a62928adb17f81a
AliReza <ali.reza.buet@gmail.com>
2023-08-28 23:49:44 +0600

    Complete the Cart

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -33,7 +33,7 @@ const CartContainer = () => {
         <footer>
             <hr />
             <div className='cart-total'>
-                <h4>total <span>${total}</span></h4>  
+                <h4>total <span>${total.toFixed(2)}</span></h4>  
             </div>
             <button className='btn clear-btn' onClick={()=> dispatch(clearCart())}>
                 clear cart
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -33,10 +33,20 @@ const cartSlice = createSlice({
             cartItem.amount = cartItem.amount - 1;
             // console.log('The cartItem', cartItem);
         },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item) => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            })
+            state.amount = amount;
+            state.total = total;
+        }
     }
 });
 
 // console.log(cartSlice)
-export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer
