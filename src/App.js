3c2f2c3c89a9bfc9cedd3aad77d44c571533253c
AliReza <ali.reza.buet@gmail.com>
2023-08-29 00:51:51 +0600

    Complete api fetching

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import Modal from './components/Modal';
 import CartContainer from './components/CartContainer';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import { useEffect } from 'react';
 
 function App() {
-  const {cartItems} = useSelector((store) => store.cart);
+  const {cartItems, isLoading} = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
   
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
 
-  const { isOpen } = useSelector((store) => store.modal);
+  useEffect(()=> {
+    dispatch(getCartItems())
+  }, []);
+
+  if(isLoading) {
+    return (
+      <div className='loading'>
+        Loading...
+      </div>
+    )
+  }
 
   return (
     <main>
