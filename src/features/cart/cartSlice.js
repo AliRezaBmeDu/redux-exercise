d11a3b946d7dc8d730e4debbf5401799384cf657
AliReza <ali.reza.buet@gmail.com>
2023-08-28 23:12:46 +0600

    Complete remove item reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -14,11 +14,17 @@ const cartSlice = createSlice({
     reducers: {
         clearCart: (state) => {
             state.cartItems = [];
+            // return {cartItems: []}
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item)=>
+            item.id !== itemId)
         },
     }
 });
 
 // console.log(cartSlice)
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer
